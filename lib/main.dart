0d33f604d64475e528e5640bc78776e1538584bd
kijiwon <kiji1313@gmail.com>
2024-08-22 18:44:10 +0900

    home widget 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonfilx/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,46 +20,7 @@ class _MyAppState extends State<MyApp> {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
-      home: const Scaffold(
-        body: Center(),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    // state 초기화
-    super.initState();
-    print('initState!');
-  }
-
-  @override
-  void dispose() {
-    // 위젯이 스크린에서 제거될 때 호출
-    super.dispose();
-    print('dispose!');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build!');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
