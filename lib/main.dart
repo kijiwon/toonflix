acc52bc9120c1d1d64d0e042061b9ae366f5d46c
kijiwon <kiji1313@gmail.com>
2024-08-22 16:11:19 +0900

    setState 함수

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:toonfilx/widgets/Button.dart';
-import 'package:toonfilx/widgets/currency_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +12,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int counter = 0; // counter는 변경된 값으로 덮어 씌워지기 때문에 final을 사용하지 않음
 
-  void onClicked() {}
+  void onClicked() {
+    // setState -> State 클래스에 데이터 변화가 있다는 것을 알리는 함수
+    setState(() {
+      counter += 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
