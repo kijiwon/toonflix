6c69c115116d45b1b32a84227b60f2162a1897a3
kijiwon <kiji1313@gmail.com>
2024-08-22 16:02:46 +0900

    IconButton 추가

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,37 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int counter = 0; // counter는 변경된 값으로 덮어 씌워지기 때문에 final을 사용하지 않음
 
+  void onClicked() {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB), // custom color
-        body: Center(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
