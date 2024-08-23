862fb36915b2cacb90fa7fe9d21f969f48e9ed15
kijiwon <kiji1313@gmail.com>
2024-08-23 16:52:53 +0900

    homeScreen AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:toonfilx/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: const HomeScreen(),
     );
   }
 }
