80eb593d1be7e06853d0a4396553206780d8262a
kijiwon <kiji1313@gmail.com>
2024-08-23 20:18:46 +0900

    data요청 방법1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:toonfilx/models/webtoon_model.dart';
+import 'package:toonfilx/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    // api요청
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {}); // state 변경
+  }
+
+  // 첫 로딩 시 데이터 요청 보내기
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
 
   @override
   Widget build(BuildContext context) {
