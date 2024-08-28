279ff5b8068bc49049828eb8a76bdef378fc2016
kijiwon <kiji1313@gmail.com>
2024-08-28 19:49:36 +0900

    webtoonDetailModel property 오타 수정

--- lib/models/webtoon_detail_model.dart
+++ lib/models/webtoon_detail_model.dart
@@ -1,9 +1,9 @@
 class WebtoonDetailModel {
-  final String title, about, gener, age;
+  final String title, about, genre, age;
 
   WebtoonDetailModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         about = json['about'],
-        gener = json['gener'],
+        genre = json['genre'],
         age = json['age'];
 }
