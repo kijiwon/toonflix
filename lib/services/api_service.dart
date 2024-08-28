84c541285f27d271f0c5aaf59d8d047af428d99f
kijiwon <kiji1313@gmail.com>
2024-08-28 18:50:23 +0900

    getLatestEpisodes 추가

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:toonfilx/models/webtoon_detail_model.dart';
+import 'package:toonfilx/models/webtoon_episode_model.dart';
 import 'package:toonfilx/models/webtoon_model.dart';
 
 class ApiService {
@@ -32,6 +33,23 @@ class ApiService {
       final webtoon = jsonDecode(response.body);
       // json화 된 데이터를 WebtoonDetailModel로 전달
       return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodes(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      // episode 하나씩 꺼내기
+      for (var episode in episodes) {
+        // episodesInstance에 추가
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+
+      return episodesInstances;
     }
     throw Error();
   }
